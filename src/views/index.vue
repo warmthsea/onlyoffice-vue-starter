58c6d1acf1708a84ddad37b87540005ddf168f2d
huayang <45450994+huayang0304@users.noreply.github.com>
2022-08-16 17:19:48 +0800

    update

---
+++ src/views/index.vue
@@ -0,0 +1,6 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>office vue</div>
+</template>
---
+++ vite.config.ts
@@ -0,0 +1,42 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: './',
+  plugins: [
+    vue(),
+    AutoImport({
+      imports: ["vue"],
+      dts: 'auto-imports.d.ts',
+    }),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    })
+  ],
+  server: {
+    host: "0.0.0.0",
+    port: 4004,
+    open: true,
+    strictPort: true,
+    hmr: {
+      overlay: false
+    },
+  },
+  build: {
+    outDir: 'docs'
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
